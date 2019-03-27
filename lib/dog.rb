aa38eef1dd75bb2c1de57ccf0e1c83c842683d16
Jennifer Joyce <jenjoyce@mac.com>
2019-03-27 00:28:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog 
   
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
   
   def name
@@ -9,7 +9,7 @@ class Dog
   end
 
 def breed=(dog_breed)
-    this_dogs_breed = dog_breed
+    @this_dogs_breed = dog_breed
   end
   
 def breed
