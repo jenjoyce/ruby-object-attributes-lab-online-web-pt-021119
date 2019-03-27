80010401a74138ca74b04578bfccaf3f0f720ad4
Jennifer Joyce <jenjoyce@mac.com>
2019-03-27 00:31:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,19 @@
 class Dog 
   
+  def initialize(name)
+    @name = name
+  end
+  
   def name=(dog_name)
     @this_dogs_name = dog_name
   end
   
   def name
     puts "#{@this_dogs_name}"
+  end
+  
+def initialize(name)
+    @name = name
   end
 
 def breed=(dog_breed)
